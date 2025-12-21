cab5bcfbdcc6ab7bb0e481f903efd2a83eaac15f
Aryan Dwivedi <aryan.735921@gmail.com>
2025-12-21 18:16:05 +0530

    routing-setup

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../components/navbar/Navbar";
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Dashboard Home</h1>
+        </div>
+    );
+}
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { Button } from '@mui/material'
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+      <Button variant='contained'>uyuyu</Button>
+    </div>
+  )
+}
